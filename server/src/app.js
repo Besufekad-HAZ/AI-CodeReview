544f91b26001611cb5c62140a2f652fdf57b2995
Besufekad-HAZ <besurye@gmail.com>
2025-03-07 13:30:53 +0300

    Install and use the CORS middleware in my Express app

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
-express = require("express");
+const express = require("express");
+const cors = require("cors"); // add this line
 const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
 
+app.use(cors()); // add this line to enable CORS
 app.use(express.json());
 
 app.get("/", (req, res) => {
