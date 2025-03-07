7f777653d4ac478a08f2d70d8114ec6934e9b45a
Besufekad-HAZ <besurye@gmail.com>
2025-03-07 11:52:30 +0300

    Export generateContent directly in ai.service.js for streamlined imports

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,7 +1,17 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+  systemInstruction: `
+     You are a code reviewer, who have experties in reviewing code.
+     You are reviewing a code written by a junior developer.
+     check the code and provide a detailed review and solution to the code.
+
+     you always try to find the best solution for the developer and also try to make the code
+     more efficient and readable. use best practices and suggest the best possible solution and clean code.
+  `,
+});
 
 // const prompt = "Explain how AI works";
 
